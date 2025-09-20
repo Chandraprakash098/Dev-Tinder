59a68aea541de6f883f57a295c845ce973547357
Chandra Prakash <chandraprakash96386@gmail.com>
2025-09-20 13:51:22 +0530

    d

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-const {validateSignupData} = require("../utils/validation");
+const { validateSignupData } = require("../utils/validation");
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 
@@ -40,54 +40,58 @@ const authController = {
       await user.save();
       res.status(200).json({ message: "User register succssfully", user });
     } catch (error) {
-      console.log(error)
+      console.log(error);
       res.status(500).json({ message: "Internal Server Error" });
     }
   },
 
-  login: async(req,res)=>{
+  login: async (req, res) => {
     try {
-        const {email, password} = req.body;
-        
-        const user = await User.findOne({email});
-        if(!user){
-            return res.status(404).json({message:"user not found"})
-        }
+      const { email, password } = req.body;
 
-        const isPassMatch= await bcrypt.compare(password, user.password)
-        if(!isPassMatch){
-          res.status("Login Failed, Password Mismatch")
-        }
-        
-        const token = jwt.sign({userId:user._id, email:user.email}, process.env.JWT_SECRET,{expiresIn:"1d"})
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(404).json({ message: "user not found" });
+      }
 
-        res.cookie("token",token,{
-          httpOnly:true,
-          secure:process.env.NODE_ENV === "production",
-          sameSite:"strict",
-          maxAge:24 * 60 * 60 * 1000, // 1 day
-        })
-        
-        res.status(200).json({
-      message: "Login Successful",
-      user: {
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-      },
-    });
-        
+      const isPassMatch = await bcrypt.compare(password, user.password);
+      if (!isPassMatch) {
+        res.status("Login Failed, Password Mismatch");
+      }
+
+      const token = jwt.sign(
+        { userId: user._id, email: user.email },
+        process.env.JWT_SECRET,
+        { expiresIn: "1d" }
+      );
+
+      res.cookie("token",token,{
+        httpOnly:true,
+        secure:process.env.NODE_ENV === "production",
+        sameSite:"strict",
+        maxAge:24 * 60 * 60 * 1000, // 1 day
+      })
+
+      
+
+      res.status(200).json({
+        message: "Login Successful",
+        user: {
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email,
+        },
+      });
     } catch (error) {
-        console.log(error)
-        res.status(500).json({message:"Login Failed, Internal Server Error"})
+      console.log(error);
+      res.status(500).json({ message: "Login Failed, Internal Server Error" });
     }
   },
 
-  logout: async(req,res)=>{
+  logout: async (req, res) => {
     res.clearCookie("token"),
-    res.status(200).json({message:"Logout Successful"})
-  }
-  
+      res.status(200).json({ message: "Logout Successful" });
+  },
 };
 
 module.exports = authController;
